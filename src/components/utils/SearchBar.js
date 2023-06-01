4f01d0fd05975f49eb57caa8ffca3ba77d2d2fad
BobWritesCode <warwick_hart@hotmail.com>
2023-06-01 17:15:16 +0100

    chore: update comments

--- src/components/utils/SearchBar.js
+++ src/components/utils/SearchBar.js
@@ -8,11 +8,11 @@ import { MockSearch } from '../../functions/MockSearch';
 const SearchBar = () => {
   // Use to control text-input
   const [content, setContent] = useState('');
-  // Use to store search results
+  // Use to API/data request results.
   const [results, setResults] = useState([]);
-  // Use to control if user clicks outside search results, to close results.
+  // Use to control if user clicks outside search bar and results and then close results.
   const searchContainerRef = useRef(null);
-  // Use to show spinner while API is resolving.
+  // Use to show spinner while API/data request is resolving.
   const [showSpinner, setShowSpinner] = useState(false);
 
   /**
@@ -27,23 +27,22 @@ const SearchBar = () => {
   };
 
   /**
-   * Handles user clicking on one of the search results.
-   * Takes user to the profile page, and clears content, and results.
-   * @param {number} id
+   * Handles user clicking on one of the results.
+   * @param {*} result
    */
-  const handleSearchResultClick = (id) => {
-    // Button action here.
+  const handleSearchResultClick = (result) => {
+    // Put your code here with what you want to happen when the result is selected.
     setContent('');
     setResults({});
     setShowSpinner(false);
   };
 
   /**
-   * Checks to see if user is clicking outside the search results, if so close results.
+   * Checks to see if user is clicking outside the search bar and results, if so close results.
    */
   useEffect(() => {
     const handleOutsideClick = (event) => {
-      // Check if the user click an element outside the search container
+      // Check if the user click an element outside the search container.
       if (
         searchContainerRef.current &&
         !searchContainerRef.current.contains(event.target)
@@ -58,15 +57,19 @@ const SearchBar = () => {
   }, []);
 
   /**
-   * Fetches search results from the API
+   * Fetches data based on text input from end user.
    */
   useEffect(() => {
+    // Check if any text in search input.
     if (content) {
+      // Show spinner while API/data request resolves.
       setShowSpinner(true);
       const fetchData = async () => {
         try {
-          // Mock API request.
+          // Mock API/data request START. -----------------
+          // Input your API/data request here.
           const data = MockSearch({ query: content });
+          // Mock API/data request END. -----------------
           setResults(data);
         } catch (err) {
           /* empty */
@@ -75,7 +78,7 @@ const SearchBar = () => {
         }
       };
 
-      // A 1 second timer, to allow user to stop typing before calling API.
+      // A 1 second timer, to allow user to stop typing before requesting data.
       const timer = setTimeout(() => {
         fetchData();
       }, 1000);
@@ -83,14 +86,13 @@ const SearchBar = () => {
         clearTimeout(timer);
       };
     } else {
-      // Remove spinner in search is empty
+      // Remove spinner in search input is empty.
       setShowSpinner(false);
     }
   }, [content]);
 
   /**
-   * JSX element to list search results as clickable buttons, that will take the user
-   * to the selected user's profile page.
+   * JSX element to list results as clickable buttons.
    */
   const ShowResults = (
     <>
@@ -110,7 +112,7 @@ const SearchBar = () => {
   );
 
   /**
-   * JSX element to show spinner until user stops typing and API resolves.
+   * JSX element to show spinner until user stops typing and API/data request resolves.
    */
   const ShowSpinner = (
     <>
@@ -128,7 +130,7 @@ const SearchBar = () => {
   );
 
   /**
-   * JSX element to act as a container for the search results.
+   * JSX element to act as a container for results.
    */
   const ShowResultsContainer = (
     <>
